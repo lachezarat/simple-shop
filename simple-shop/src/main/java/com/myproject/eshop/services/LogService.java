ad2122d79ee2aae715c6600377901bdebaefea64
lucho <l.atanasov3333@gmail.com>
2020-02-11 22:02:27 +0200

    Logger added

---
+++ simple-shop/src/main/java/com/myproject/eshop/services/LogService.java
@@ -0,0 +1,8 @@
+package com.myproject.eshop.services;
+
+import com.myproject.eshop.data.models.service.LogServiceModel;
+
+public interface LogService {
+
+    LogServiceModel seedLogInDb(LogServiceModel logServiceModel);
+}
