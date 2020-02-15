a07c420237b603278beafd1b354c9e486b5568d2
lucho <l.atanasov3333@gmail.com>
2020-02-15 20:29:57 +0200

    Added login authentication

--- simple-shop/src/main/java/com/myproject/eshop/config/ApplicationSecurityConfiguration.java
+++ simple-shop/src/main/java/com/myproject/eshop/config/ApplicationSecurityConfiguration.java
@@ -1,19 +1,26 @@
 package com.myproject.eshop.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().disable()
-                .csrf().disable()
+                .cors()
+                .disable()
+                .csrf()
+                .csrfTokenRepository(csrfTokenRepository())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "/register", "/login").anonymous()
                 .anyRequest().authenticated()
@@ -26,5 +33,12 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .and()
                 .logout()
                 .logoutSuccessUrl("/");
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository =
+                new HttpSessionCsrfTokenRepository();
+        repository.setSessionAttributeName("_csrf");
+        return repository;
     }
 }
