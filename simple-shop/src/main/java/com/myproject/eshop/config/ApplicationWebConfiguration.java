cdaf0e7ee71f3eeaee40caaf22f17e64e4456f10
lucho <l.atanasov3333@gmail.com>
2020-03-07 22:34:18 +0200

    Fixed page bugs

--- simple-shop/src/main/java/com/myproject/eshop/config/ApplicationWebConfiguration.java
+++ simple-shop/src/main/java/com/myproject/eshop/config/ApplicationWebConfiguration.java
@@ -1,6 +1,7 @@
 package com.myproject.eshop.config;
 
 import com.myproject.eshop.web.interceptors.FaviconInterceptor;
+import com.myproject.eshop.web.interceptors.LogoInterceptor;
 import com.myproject.eshop.web.interceptors.PageTitleInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +13,19 @@ public class ApplicationWebConfiguration implements WebMvcConfigurer {
 
     private final FaviconInterceptor faviconInterceptor;
     private final PageTitleInterceptor pageTitleInterceptor;
+    private final LogoInterceptor logoInterceptor;
 
     @Autowired
-    public ApplicationWebConfiguration(FaviconInterceptor faviconInterceptor, PageTitleInterceptor pageTitleInterceptor) {
+    public ApplicationWebConfiguration(FaviconInterceptor faviconInterceptor, PageTitleInterceptor pageTitleInterceptor, LogoInterceptor logoInterceptor) {
         this.faviconInterceptor = faviconInterceptor;
         this.pageTitleInterceptor = pageTitleInterceptor;
+        this.logoInterceptor = logoInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(faviconInterceptor);
         registry.addInterceptor(pageTitleInterceptor);
+        registry.addInterceptor(logoInterceptor);
     }
 }
