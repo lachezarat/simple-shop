6067a37f15ef79f8f3b6e084028894714d7c2127
lucho <l.atanasov3333@gmail.com>
2020-03-30 22:36:36 +0300

    Fixed logo and favicon showing

--- simple-shop/src/main/java/com/myproject/eshop/config/ApplicationWebConfiguration.java
+++ simple-shop/src/main/java/com/myproject/eshop/config/ApplicationWebConfiguration.java
@@ -1,7 +1,5 @@
 package com.myproject.eshop.config;
 
-import com.myproject.eshop.web.interceptors.FaviconInterceptor;
-import com.myproject.eshop.web.interceptors.LogoInterceptor;
 import com.myproject.eshop.web.interceptors.PageTitleInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -11,21 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ApplicationWebConfiguration implements WebMvcConfigurer {
 
-    private final FaviconInterceptor faviconInterceptor;
     private final PageTitleInterceptor pageTitleInterceptor;
-    private final LogoInterceptor logoInterceptor;
 
     @Autowired
-    public ApplicationWebConfiguration(FaviconInterceptor faviconInterceptor, PageTitleInterceptor pageTitleInterceptor, LogoInterceptor logoInterceptor) {
-        this.faviconInterceptor = faviconInterceptor;
+    public ApplicationWebConfiguration(PageTitleInterceptor pageTitleInterceptor) {
         this.pageTitleInterceptor = pageTitleInterceptor;
-        this.logoInterceptor = logoInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(faviconInterceptor);
         registry.addInterceptor(pageTitleInterceptor);
-        registry.addInterceptor(logoInterceptor);
     }
 }
